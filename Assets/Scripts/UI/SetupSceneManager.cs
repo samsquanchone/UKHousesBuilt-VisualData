2c7db5b3ebe6b6321772468566f6a155eee816c2
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2022-11-10 19:17:44 +0000

    Clamp max value fixed
    noticed to string for max was set to int value for min. So just changed that.

--- Assets/Scripts/UI/SetupSceneManager.cs
+++ Assets/Scripts/UI/SetupSceneManager.cs
@@ -49,7 +49,7 @@ public class SetupSceneManager : MonoBehaviour
         else 
         {
             max_salary_int = Mathf.Clamp(max_salary_int, 100000, 1000000);
-            input_max_salary.text = min_salary_int.ToString();
+            input_max_salary.text = max_salary_int.ToString();
         }
        
         if (float.TryParse(input_max_population.text, out float max_population_int) != true)
