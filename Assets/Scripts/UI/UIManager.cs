c3d22b8eae702139e3a6565916256f7fb156c8d5
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2022-11-10 14:29:34 +0000

    Simulation basis added

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Slider yearSlider;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Timer()
+    {
+        if (yearSlider.value <= 2020)
+        {
+            yearSlider.value = yearSlider.value + 1;
+
+            yield return new WaitForSeconds(5f);
+
+            Timer();
+        }
+    }
+
+    public void StartSimulation()
     {
         
     }
